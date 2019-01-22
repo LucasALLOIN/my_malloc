e6d3442bc2826d565a805769ec3a26f235b3a654
qdieppe <quentin.dieppe@epitech.eu>
2019-01-22 14:30:11 +0100

    rnb

---
+++ rnb_trees/source/violation.c
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2019
+** source
+** File description:
+** violation
+*/
+
+#include "violation.h"
+
+rnb_violation_error_t isViolationRootRed(rnb_node_t **root, rnb_node_t *grandfather)
+{
+
+}
+
+rnb_violation_error_t *isViolationDoubleRed(rnb_node_t **root, rnb_node_t *grandfather)
+{
+
+}
+
+rnb_violation_error_t *isViolation()
+{
+
+}
