c0cdd08ee97e94e9c1193b7cded560349856ab66
Lucas ALLOIN <lucas.alloin@epitech.eu>
2019-02-03 21:08:21 +0100

    norme again again

--- rnb_trees/rnb_violation.c
+++ rnb_trees/rnb_violation.c
@@ -21,7 +21,8 @@ rnb_violation_type_t is_violation_root_red(rnb_node_t **root)
     return (NO_ERROR);
 }
 
-int get_number_of_blacks_node_split(rnb_node_t *root, int num, int num2, rnb_node_t **err)
+int get_number_of_blacks_node_split(rnb_node_t *root, int num, int num2,
+rnb_node_t **err)
 {
     int ret = 0;
     ret = ((num2 == num) ? num : -1);
@@ -51,7 +52,7 @@ int get_number_of_black_nodes(rnb_node_t *root, int value, rnb_node_t **err)
         return (num);
     if (root->left == NULL && root->right != NULL)
         return (num2);
-    return(get_number_of_blacks_node_split(root, num, num2, err));
+    return (get_number_of_blacks_node_split(root, num, num2, err));
 }
 
 void preset_error(rnb_node_t **root, rnb_violation_error_t *error,
@@ -72,5 +73,30 @@ rnb_node_t *chield)
         error->_father = NULL;
         error->_grandfather = NULL;
         error->_uncle = NULL;
+    }
+}
+
+void dump_violation_error(rnb_violation_error_t *error)
+{
+    if (error->_type == CONSECUTIVE_RED) {
+        printf("CONSECUTIVE_RED\n");
+        printf("SHAPE : %s\n",
+        (get_shape(error->_root, error->_chield) == LINE) ? "LINE" : "TRIANGL");
+        printf("chield :\n");
+        dump_node(error->_chield);
+        printf("father :\n");
+        dump_node(error->_father);
+        printf("grandfather :\n");
+        dump_node(error->_grandfather);
+        printf("uncle :\n");
+        dump_node(error->_uncle);
+    } else if (error->_type == TWO_MUCH_BLACK_NODE) {
+        printf("TWO_MUCH_BLACK_NODE\n");
+        printf("SHAPE : %s",
+        (get_shape(error->_root, error->_chield) == LINE) ? "LINE" : "TRIANGL");
+        printf(" :\n");
+        dump_node(error->_chield);
+    } else if (error->_type == ROOT_RED) {
+        printf("ROOT RED\n");
     }
 }
